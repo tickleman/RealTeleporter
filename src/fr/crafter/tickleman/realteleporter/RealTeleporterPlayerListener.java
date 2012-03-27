58b9fed964e401a3ee5eb5912eeda7c8301ce062
tickleman <mc@thgbm.net>
2012-03-27 23:58:56 +0200

    - now accept translation files - now accept "bukkit" or "permissions" permissions - now information "teleport from xxx to xxx" when taking a gate => release 1.01

--- src/fr/crafter/tickleman/realteleporter/RealTeleporterPlayerListener.java
+++ src/fr/crafter/tickleman/realteleporter/RealTeleporterPlayerListener.java
@@ -30,49 +30,58 @@ public class RealTeleporterPlayerListener implements Listener
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event)
 	{
-		long time = new Date().getTime();
-		Long next = nextCheck.get(event.getPlayer());
-		if (next == null) {
-			nextCheck.put(event.getPlayer(), next = (long)0);
-		}
-		if (time > next) {
-			nextCheck.put(event.getPlayer(), time + 100);
-			Player player = event.getPlayer();
-			String playerName = player.getName();
-			RealTeleporter teleporter = plugin.teleporters.teleporterAt(player);
-			if ((teleporter != null) && (teleporter.target != null)) {
-				Location playerLocation = player.getLocation();
-				String key = RealLocation.getId(playerLocation);
-				if (!key.equals(plugin.playerLocation.get(playerName))) {
-					plugin.playerLocation.put(playerName, key);
-					RealTeleporter target = teleporter.target;
-					for (World world : plugin.getServer().getWorlds()) {
-						if (world.getName().equals(target.worldName)) {
-							float yaw;
-							switch (target.direction) {
-								case 'E': yaw = 180; break;
-								case 'S': yaw = 270; break;
-								case 'W': yaw = 0; break;
-								default:  yaw = 90; break;
+		Player player = event.getPlayer();
+		if (plugin.hasPermission(player, "realteleporter.teleport")) {
+			long time = new Date().getTime();
+			Long next = nextCheck.get(event.getPlayer());
+			if (next == null) {
+				nextCheck.put(player, next = (long)0);
+			}
+			if (time > next) {
+				nextCheck.put(player, time + 100);
+				String playerName = player.getName();
+				RealTeleporter teleporter = plugin.teleporters.teleporterAt(player);
+				if ((teleporter != null) && (teleporter.target != null)) {
+					Location playerLocation = player.getLocation();
+					String key = RealLocation.getId(playerLocation);
+					if (!key.equals(plugin.playerLocation.get(playerName))) {
+						plugin.playerLocation.put(playerName, key);
+						RealTeleporter target = teleporter.target;
+						for (World world : plugin.getServer().getWorlds()) {
+							if (world.getName().equals(target.worldName)) {
+								float yaw;
+								switch (target.direction) {
+									case 'E': yaw = 180; break;
+									case 'S': yaw = 270; break;
+									case 'W': yaw = 0; break;
+									default:  yaw = 90; break;
+								}
+								Location location = new Location(
+									world, target.x + .5, target.y, target.z + .5, yaw, 0
+								);
+								plugin.getLog().info(
+									"<" + playerName + "> from "
+									+ teleporter.name
+									+ " to " + target.name + " ("
+									+ target.worldName + "," + target.x + "," + target.y + "," + target.z + "," + yaw
+									+ ")"
+								);
+								player.teleport(location);
+								event.setTo(location);
+								plugin.playerLocation.put(playerName, target.getLocationKey());
+								if (plugin.hasPermission(player, "realteleporter.teleport.showgatename")) {
+									player.sendMessage(
+										plugin.tr("Teleport from +1 to +2")
+										.replace("+1", teleporter.name)
+										.replace("+2", target.name)
+									);
+								}
 							}
-							Location location = new Location(
-								world, target.x + .5, target.y, target.z + .5, yaw, 0
-							);
-							plugin.getLog().info(
-								"<" + playerName + "> from "
-								+ teleporter.name
-								+ " to " + target.name + " ("
-								+ target.worldName + "," + target.x + "," + target.y + "," + target.z + "," + yaw
-								+ ")"
-							);
-							player.teleport(location);
-							event.setTo(location);
-							plugin.playerLocation.put(playerName, target.getLocationKey());
 						}
 					}
+				} else if (plugin.playerLocation.get(playerName) != null) {
+					plugin.playerLocation.remove(playerName);
 				}
-			} else if (plugin.playerLocation.get(playerName) != null) {
-				plugin.playerLocation.remove(playerName);
 			}
 		}
 	}
--- src/fr/crafter/tickleman/realteleporter/RealTeleporterPlugin.java
+++ src/fr/crafter/tickleman/realteleporter/RealTeleporterPlugin.java
@@ -37,116 +37,153 @@ public class RealTeleporterPlugin extends RealPlugin
 			String command = cmd.getName().toLowerCase();
 			Player player = (Player)sender;
 			Location location = player.getLocation();
-			if (command.equals("rtel") || command.equals("rt")) {
+			if (
+				(command.equals("rtel") || command.equals("rt"))
+				&& hasPermission(player, "realteleporter")
+			) {
 				String param1 = args.length > 0 ? args[0] : "";
 				String param2 = args.length > 1 ? args[1] : "";
 				String param3 = args.length > 2 ? args[2] : "";
-				if (param1.equals("create") || param1.equals("c")) {
-					// create
-					if (!param2.equals("") && teleporters.byName.get(param2) == null) {
-						RealTeleporter teleporter = new RealTeleporter(
-							param2,
-							location.getWorld().getName(),
-							Math.round(Math.floor(location.getX())),
-							Math.round(Math.floor(location.getY())),
-							Math.round(Math.floor(location.getZ())),
-							location.getYaw()
-						);
-						teleporters.byName.put(teleporter.name, teleporter);
-						teleporters.byLocation.put(teleporter.getLocationKey(), teleporter);
-						teleporters.save();
-						player.sendMessage("Create teleporter " + teleporter.name);
-					} else {
-						if (param2.equals("")) {
-							player.sendMessage("You must give the teleporter a name");
+				param1 = paramShortCut(param1);
+				if (hasPermission(player, "realteleporter." + param1)) {
+					if (param1.equals("create")) {
+						// create
+						if (!param2.equals("") && teleporters.byName.get(param2) == null) {
+							RealTeleporter teleporter = new RealTeleporter(
+								param2,
+								location.getWorld().getName(),
+								Math.round(Math.floor(location.getX())),
+								Math.round(Math.floor(location.getY())),
+								Math.round(Math.floor(location.getZ())),
+								location.getYaw()
+							);
+							teleporters.byName.put(teleporter.name, teleporter);
+							teleporters.byLocation.put(teleporter.getLocationKey(), teleporter);
+							teleporters.save();
+							player.sendMessage(tr("Create teleporter +1").replace("+1", teleporter.name));
 						} else {
-							player.sendMessage("Teleporter " + param2 + " alreadty exist");
+							if (param2.equals("")) {
+								player.sendMessage(tr("You must give the teleporter a name"));
+							} else {
+								player.sendMessage(tr("Teleporter +1 already exist").replace("+1", param2));
+							}
 						}
-					}
-					return true;
-				} else if (param1.equals("remove") || param1.equals("r")) {
-					// remove
-					RealTeleporter teleporter = teleporters.byName.get(param2);
-					if (teleporter != null) {
-						teleporters.byName.remove(teleporter.name);
-						teleporters.byLocation.remove(teleporter.getLocationKey());
-						teleporters.save();
-						player.sendMessage("Remove teleporter " + teleporter.name);
-					} else {
-						player.sendMessage("Teleporter " + param2 + " does not exist");
-					}
-					return true;
-				} else if (param1.equals("link")) {
-					// link
-					RealTeleporter teleporter1 = teleporters.byName.get(param2);
-					RealTeleporter teleporter2 = teleporters.byName.get(param3);
-					if (teleporter1 != null && teleporter2 != null) {
-						teleporter1.setTarget(teleporter2);
-						teleporters.save();
-						player.sendMessage("Link teleporter " + teleporter1.name + " to " + teleporter2.name);
-					} else {
-						if (teleporter1 == null) player.sendMessage("Teleporter " + param2 + " does not exist");
-						if (teleporter2 == null) player.sendMessage("Teleporter " + param3 + " does not exist");
-					}
-					return true;
-				} else if (param1.equals("loop") || param1.equals("l")) {
-					// loop link
-					RealTeleporter teleporter1 = teleporters.byName.get(param2);
-					RealTeleporter teleporter2 = teleporters.byName.get(param3);
-					if (teleporter1 != null && teleporter2 != null) {
-						teleporter1.setTarget(teleporter2);
-						teleporter2.setTarget(teleporter1);
-						teleporters.save();
-						player.sendMessage("Link teleporter " + teleporter1.name + " to " + teleporter2.name);
-						player.sendMessage("Link teleporter " + teleporter2.name + " to " + teleporter1.name);
-					} else {
-						if (teleporter1 == null) player.sendMessage("Teleporter " + param2 + " does not exist");
-						if (teleporter2 == null) player.sendMessage("Teleporter " + param3 + " does not exist");
-					}
-					return true;
-				} else if (param1.equals("unlink")) {
-					// unlink
-					RealTeleporter teleporter = teleporters.byName.get(param2);
-					if (teleporter != null) {
-						teleporter.setTarget(null);
-						teleporters.save();
-						player.sendMessage("Unlinked teleporter " + teleporter.name);
-					} else {
-						player.sendMessage("Teleporter " + param2 + " does not exist");
-					}
-					return true;
-				} else if (param1.equals("unloop") || param1.equals("u")) {
-					// unloop
-					RealTeleporter teleporter1 = teleporters.byName.get(param2);
-					RealTeleporter teleporter2 = null;
-					if (teleporter1 != null) {
-						teleporter2 = teleporter1.target;
-						teleporter1.setTarget(null);
-						player.sendMessage("Unlinked teleporter " + teleporter1.name);
-					}
-					if (teleporter2 != null) {
-						teleporter2.setTarget(null);
-						player.sendMessage("Unlinked teleporter " + teleporter2.name);
-					}
-					if (teleporter1 != null || teleporter2 != null) {
-						teleporters.save();
-					} else {
-						player.sendMessage("Teleporter " + param2 + " does not exist");
-					}
-					return true;
-				} else if (param1.equals("list")) {
-					// list teleporters
-					String values = "Gates list :";
-					for (RealTeleporter teleporter : teleporters.byName.values()) {
-						values = values + " " + teleporter.name;
-					}
-					player.sendMessage(values);
-					return true;
-				} else if (param1.equals("orphan") || param1.equals("o")) {
-					// list orphan teleporters
-					String values = "Orphan gates :";
-					for (RealTeleporter teleporter1 : teleporters.byName.values()) {
-						if (teleporter1.target == null) {
+						return true;
+					} else if (param1.equals("remove")) {
+						// remove
+						RealTeleporter teleporter = teleporters.byName.get(param2);
+						if (teleporter != null) {
+							teleporters.byName.remove(teleporter.name);
+							teleporters.byLocation.remove(teleporter.getLocationKey());
+							teleporters.save();
+							player.sendMessage(tr("Remove teleporter +1").replace("+1", teleporter.name));
+						} else {
+							player.sendMessage(tr("Teleporter +1 does not exist").replace("+1", param2));
+						}
+						return true;
+					} else if (param1.equals("link")) {
+						// link
+						RealTeleporter teleporter1 = teleporters.byName.get(param2);
+						RealTeleporter teleporter2 = teleporters.byName.get(param3);
+						if (teleporter1 != null && teleporter2 != null) {
+							teleporter1.setTarget(teleporter2);
+							teleporters.save();
+							player.sendMessage(
+								tr("Link teleporter +1 to +2")
+								.replace("+1",teleporter1.name)
+								.replace("+2", teleporter2.name)
+							);
+						} else {
+							if (teleporter1 == null) player.sendMessage(tr("Teleporter +1 does not exist").replace("+1", param2));
+							if (teleporter2 == null) player.sendMessage(tr("Teleporter +1 does not exist").replace("+2", param3));
+						}
+						return true;
+					} else if (param1.equals("loop")) {
+						// loop link
+						RealTeleporter teleporter1 = teleporters.byName.get(param2);
+						RealTeleporter teleporter2 = teleporters.byName.get(param3);
+						if (teleporter1 != null && teleporter2 != null) {
+							teleporter1.setTarget(teleporter2);
+							teleporter2.setTarget(teleporter1);
+							teleporters.save();
+							player.sendMessage(tr("Link teleporter +1 to +2").replace("+1", teleporter1.name).replace("+2", teleporter2.name));
+							player.sendMessage(tr("Link teleporter +1 to +2").replace("+1", teleporter2.name).replace("+2", teleporter1.name));
+						} else {
+							if (teleporter1 == null) player.sendMessage(tr("Teleporter +1 does not exist").replace("+1", param2));
+							if (teleporter2 == null) player.sendMessage(tr("Teleporter +1 does not exist").replace("+1", param3));
+						}
+						return true;
+					} else if (param1.equals("unlink")) {
+						// unlink
+						RealTeleporter teleporter = teleporters.byName.get(param2);
+						if (teleporter != null) {
+							teleporter.setTarget(null);
+							teleporters.save();
+							player.sendMessage(tr("Unlinked teleporter +1").replace("+1", teleporter.name));
+						} else {
+							player.sendMessage(tr("Teleporter +1").replace("+1", param2));
+						}
+						return true;
+					} else if (param1.equals("unloop")) {
+						// unloop
+						RealTeleporter teleporter1 = teleporters.byName.get(param2);
+						RealTeleporter teleporter2 = null;
+						if (teleporter1 != null) {
+							teleporter2 = teleporter1.target;
+							teleporter1.setTarget(null);
+							player.sendMessage(tr("Unlinked teleporter +1").replace("+1", teleporter1.name));
+						}
+						if (teleporter2 != null) {
+							teleporter2.setTarget(null);
+							player.sendMessage(tr("Unlinked teleporter +1").replace("+1", teleporter2.name));
+						}
+						if (teleporter1 != null || teleporter2 != null) {
+							teleporters.save();
+						} else {
+							player.sendMessage(tr("Teleporter +1 does not exist").replace("+1", param2));
+						}
+						return true;
+					} else if (param1.equals("list")) {
+						// list teleporters
+						String values = tr("Gates list") + " :";
+						for (RealTeleporter teleporter : teleporters.byName.values()) {
+							values = values + " " + teleporter.name;
+						}
+						player.sendMessage(values);
+						return true;
+					} else if (param1.equals("orphan")) {
+						// list orphan teleporters
+						String values = tr("Orphan gates") + " :";
+						for (RealTeleporter teleporter1 : teleporters.byName.values()) {
+							if (teleporter1.target == null) {
+								boolean found = false;
+								for (RealTeleporter teleporter2 : teleporters.byName.values()) {
+									if (teleporter2.targetName.equals(teleporter1.name)) {
+										found = true;
+										break;
+									}
+								}
+								if (!found) {
+									values = values + " " + teleporter1.name;
+								}
+							}
+						}
+						player.sendMessage(values);
+						return true;
+					} else if (param1.equals("withouttarget")) {
+						// list teleporters without target
+						String values = tr("Gates without target") + " :";
+						for (RealTeleporter teleporter : teleporters.byName.values()) {
+							if (teleporter.target == null) {
+								values = values + " " + teleporter.name;
+							}
+						}
+						player.sendMessage(values);
+						return true;
+					} else if (param1.equals("withoutsource")) {
+						// list teleporters without source
+						String values = tr("Gates without source") + " :";
+						for (RealTeleporter teleporter1 : teleporters.byName.values()) {
 							boolean found = false;
 							for (RealTeleporter teleporter2 : teleporters.byName.values()) {
 								if (teleporter2.targetName.equals(teleporter1.name)) {
@@ -158,64 +195,69 @@ public class RealTeleporterPlugin extends RealPlugin
 								values = values + " " + teleporter1.name;
 							}
 						}
-					}
-					player.sendMessage(values);
-					return true;
-				} else if (param1.equals("withouttarget") || param1.equals("wt")) {
-					// list teleporters without target
-					String values = "Gates without target :";
-					for (RealTeleporter teleporter : teleporters.byName.values()) {
-						if (teleporter.target == null) {
-							values = values + " " + teleporter.name;
+						player.sendMessage(values);
+						return true;
+					} else if (param1.equals("nearest")) {
+						int count;
+						try { count = Integer.parseInt(param2); } catch (Exception e) { count = 5; }
+						for (
+							RealTeleporter teleporter :
+								teleporters.getSortedByDistance(player.getLocation()).values()
+						) {
+							player.sendMessage(teleporter.toString());
+							if (count-- == 0) break;
 						}
+						return true;
+					} else if (param1.equals("help")) {
+						player.sendMessage(tr("/rtel create <gate> : create a gate"));
+						player.sendMessage(tr("/rtel remove <gate> : remove gate"));
+						player.sendMessage(tr("/rtel link <gate1> <gate2> : link a gate to another"));
+						player.sendMessage(tr("/rtel loop <gate1> <gate2> : loop two gates"));
+						player.sendMessage(tr("/rtel unlink <gate1> : remove link from gate"));
+						player.sendMessage(tr("/rtel unloop <gate1> : remove link from and its destination"));
+						player.sendMessage(tr("/rtel list : full gates list"));
+						player.sendMessage(tr("/rtel orphan : gates without source nor target list"));
+						player.sendMessage(tr("/rtel withouttarget : gates without target list"));
+						player.sendMessage(tr("/rtel withoutsource : gates without source list"));
+						player.sendMessage(tr("/rtel nearest : gates near from you"));
+						return true;
 					}
-					player.sendMessage(values);
-					return true;
-				} else if (param1.equals("withoutsource") || param1.equals("ws")) {
-					// list teleporters without source
-					String values = "Gates without source :";
-					for (RealTeleporter teleporter1 : teleporters.byName.values()) {
-						boolean found = false;
-						for (RealTeleporter teleporter2 : teleporters.byName.values()) {
-							if (teleporter2.targetName.equals(teleporter1.name)) {
-								found = true;
-								break;
-							}
-						}
-						if (!found) {
-							values = values + " " + teleporter1.name;
-						}
-					}
-					player.sendMessage(values);
-					return true;
-				} else if (param1.equals("nearest")) {
-					int count;
-					try { count = Integer.parseInt(param2); } catch (Exception e) { count = 5; }
-					for (
-						RealTeleporter teleporter :
-							teleporters.getSortedByDistance(player.getLocation()).values()
-					) {
-						player.sendMessage(teleporter.toString());
-						if (count-- == 0) break;
-					}
-					return true;
-				} else if (param1.equals("help") || param1.equals("h")) {
-					player.sendMessage("/rtel create <gate> : create a gate");
-					player.sendMessage("/rtel remove <gate> : remove gate");
-					player.sendMessage("/rtel link <gate1> <gate2> : link a gate to another");
-					player.sendMessage("/rtel loop <gate1> <gate2> : loop two gates");
-					player.sendMessage("/rtel unlink <gate1> : remove link from gate");
-					player.sendMessage("/rtel unloop <gate1> : remove link from and its destination");
-					player.sendMessage("/rtel list : full gates list");
-					player.sendMessage("/rtel orphan : gates without source nor target list");
-					player.sendMessage("/rtel withouttarget : gates without target list");
-					player.sendMessage("/rtel withoutsource : gates without source list");
-					player.sendMessage("/rtel nearest : gates near from you");
+				} else {
+					player.sendMessage(tr("You don't have access to this command"));
 					return true;
 				}
 			}
 		}
 		return false;
+	}
+
+	//--------------------------------------------------------------------------------- paramShortCut
+	public String paramShortCut(String param)
+	{
+		if (param.length() > 2) return param;
+		if (param.equals("c")) return "create";
+		if (param.equals("r")) return "remove";
+		if (param.equals("lk")) return "link";
+		if (param.equals("l") || param.equals("lp")) return "loop";
+		if (param.equals("uk")) return "unlink";
+		if (param.equals("u")) return "unloop";
+		if (param.equals("o")) return "orphan";
+		if (param.equals("wt")) return "withouttarget";
+		if (param.equals("ws")) return "withoutsource";
+		if (param.equals("n")) return "nearest";
+		if (param.equals("h")) return "help";
+		return param;
+	}
+
+	//--------------------------------------------------------------------------- playerHasPermission
+	/**
+	 * Plugin developer can override this to set default non-op players some permissions
+	 * if permission system is "none"
+	 */
+	@Override
+	public boolean playerHasPermission(String permissionString)
+	{
+		return permissionString.equals("realteleporter.teleport");
 	}
 
 	//------------------------------------------------------------------------------------- onDisable
